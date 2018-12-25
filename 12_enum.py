03e6653d7317272e5dcb4be51c743964c64cdc8a
lixiang <lixiang@163.com>
2018-12-25 13:04:50 +0800

    暂存

--- 12_enum.py
+++ 12_enum.py
@@ -7,6 +7,25 @@ from enum import Enum
 Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
 
 if __name__ == "__main__":
+    '''
+    Enum中的__members__就是一个特殊的方法
+    '''
+    '''@property
+    def __members__(cls):
+        """Returns a mapping of member name->value.
+        This mapping lists all enum members, including aliases. Note that this
+        is a read-only view of the internal mapping.
+        """
+        return MappingProxyType(cls._member_map_)
+    返回成员名称 - >值的映射。
+         此映射列出了所有枚举成员，包括别名。 请注意这一点
+         是内部映射的只读视图。
+    '''
+    ''''
+        enum_class._member_map_ = OrderedDict()      # name->value map
+    ''''
     for name,member in Month.__members__.items():
+        print(name,'=>',member,',',member.value)
+    for name,member in Month.__members__:
         print(name,'=>',member,',',member.value)
 
---
+++ metaclass_test.py
@@ -0,0 +1,7 @@
+from utils.hello import Hello
+
+h = Hello()
+print("Hello() : ",type(Hello()))
+print("Hello : ",type(Hello))
+print("h.hello :",h.hello)
+print("h :",type(h))
