eb9def52350ac23970943d86be433dbc9d26773a
lixiang <lx02140105@163.com>
2018-12-27 13:07:11 +0800

    暂存

---
+++ 15_multiprocessing_process.py
@@ -0,0 +1,20 @@
+'''
+多进程 - Windows
+    Python的multiprocessing模块提供了一个Process类来代表一个进程对象
+    multiprocessing模块是跨平台版本的多进程模块。
+
+问题：
+    安装multiprocessing时，遇到了一个都会遇到的问题
+    SyntaxError: Missing parentheses in call to 'print'. Did you mean print(int 'Macros:')?
+    原因是python3.x和Python2.x兼容的问题
+'''
+from multiprocessing import Process
+import os
+
+def run_process(name):
+    print("子进程为：%s, %s" % (name,os.getpid()))
+
+if __name__ == "__main__":
+    print("父进程为：%s, %s" % (name, os.getpid()))
+
+    child = Process(target)
